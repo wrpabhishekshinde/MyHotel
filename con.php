306c2cb49a0d963729e90d947e3b6b5dfae0dbd0
abhishek shinde <wrpabhishekshinde@gmail.com>
2024-06-13 22:47:29 +0530

    Update con.php
    commented print_r()

--- con.php
+++ con.php
@@ -2,7 +2,7 @@
 $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "certificate.pem", NULL, NULL);
 mysqli_real_connect($con, "frtdbserver74.mysql.database.azure.com", "gsshinde", "Pass@123", "fin_demo", 3306, MYSQLI_CLIENT_SSL);
-print_r($con);
+//print_r($con);
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
   }
