a73ded7103f86873510874da6ebe2ee789adcded
abhishek shinde <wrpabhishekshinde@gmail.com>
2024-06-15 04:09:38 +0530

    Update chatbot.php

--- chatbot.php
+++ chatbot.php
@@ -32,13 +32,13 @@ userArea();
             display: flex;
             justify-content: center;
             align-items: center;
-            height: 100%;
+            height: 80%;
             width: 100%;
-            button:-80px;
+           
         }
         #azurebotContainer {
-            width: 100%;
-            height: 400px;
+            width: 80%;
+            height: 500px;
             max-width: 500px;
             box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
             background-color: white;
